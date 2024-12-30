19cef2ecd6dd69beb09327df1bd472c135d66546
Stephen Beitzel <sbeitzel@pobox.com>
2024-12-30 11:08:50 -0800

    Initial Commit

---
+++ SDTCARFDemo/SDTCARFDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDTCARFDemoApp.swift
+//  SDTCARFDemo
+//
+//  Created by Stephen Beitzel on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SDTCARFDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SDTCARFDemoTests/SDTCARFDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SDTCARFDemoTests.swift
+//  SDTCARFDemoTests
+//
+//  Created by Stephen Beitzel on 12/30/24.
+//
+
+import Testing
+@testable import SDTCARFDemo
+
+struct SDTCARFDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
